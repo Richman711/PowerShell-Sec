f25b389a884c8ece7a5147d8fabfd4f14aeb4226
Richman711 <Richorman@gmail.com>
2018-01-29 00:09:17 +0900

    Add files via upload

---
+++ Get-MyStringToSHA512.ps1
@@ -0,0 +1,26 @@
+﻿Function Get-MyStringToSHA512 { 
+
+    Param(
+        [Parameter(ValueFromPipeline=$True,
+                   ValueFromPipelineByPropertyName=$True,
+                   Mandatory=$True)]
+        [String]$String,
+
+        [ValidateSet('SHA256','SHA-256','SHA384','SHA-384','SHA512','SHA-512')]
+        [string]$HashName = "SHA512"
+    )
+
+    BEGIN {}
+
+    PROCESS {
+        $StringBuilder = New-Object System.Text.StringBuilder 
+        [System.Security.Cryptography.HashAlgorithm]::Create($HashName).`
+            ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String))| ForEach-Object{ 
+            [Void]$StringBuilder.Append($PSItem.ToString("x2")) 
+            } 
+        $StringBuilder.ToString() 
+        } #PROCESS
+
+    END{}
+
+} #End Function Get-MyStringToSHA512
